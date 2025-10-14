b9287ff0fe31447511af3247d08319c63acd7c9b
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-10-13 20:10:06 -0700

    avance formulario

---
+++ assets/js/formulario.js
@@ -0,0 +1,22 @@
+function onlyLetters(event) {
+
+    const pressedKey = event.key;
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    if (!letters.includes(pressedKey)) {
+        return false
+    }
+
+    function validateEmail(email) {
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        return emailPattern.test(email);
+
+    }
+
+}
