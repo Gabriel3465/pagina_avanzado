8a5c883c06f16abe0f80964664295aed35d2a74e
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-10-16 22:31:39 -0700

    avance de  To-Do

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -2,10 +2,10 @@ function onlyLetters(event) {
 
     const pressedKey = event.key;
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
-        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p',
         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
     if (!letters.includes(pressedKey)) {
         return false
